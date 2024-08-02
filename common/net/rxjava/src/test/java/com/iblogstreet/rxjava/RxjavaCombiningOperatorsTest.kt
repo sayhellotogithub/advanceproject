be8b927e4aeab23b0c5d795f72f7c7ff9abc22de
junwang <wang.jun16@163.com>
2024-08-02 23:41:10 +0900

    add withLatestFrom,sample,ambWith,reduce,scan test

--- common/net/rxjava/src/test/java/com/iblogstreet/rxjava/RxjavaCombiningOperatorsTest.kt
+++ common/net/rxjava/src/test/java/com/iblogstreet/rxjava/RxjavaCombiningOperatorsTest.kt
@@ -105,8 +105,9 @@ class RxjavaCombiningOperatorsTest {
         firstQueue.onNext(5)
 
     }
+
     @Test
-    fun zipTest(){
+    fun zipTest() {
         val compositeDisposable = CompositeDisposable()
         val firstQueue = PublishSubject.create<Int>()
         val secondQueue = PublishSubject.create<Int>()
@@ -123,4 +124,115 @@ class RxjavaCombiningOperatorsTest {
         secondQueue.onNext(4)
         firstQueue.onNext(5)
     }
+
+    @Test
+    fun withLatestFromTest() {
+        val compositeDisposable = CompositeDisposable()
+        val button = PublishSubject.create<Unit>()
+        val editText = PublishSubject.create<String>()
+
+        button.withLatestFrom(editText) { _: Unit, value: String ->
+            value
+
+        }.subscribeBy {
+            println(it)
+        }.addTo(compositeDisposable)
+
+        button.onNext(Unit)
+        editText.onNext("a")
+        editText.onNext("ab")
+        button.onNext(Unit)
+        button.onNext(Unit)
+        editText.onNext("abc")
+
+    }
+
+    @Test
+    fun sampleTest() {
+        // You could have achieved the same behavior by adding a distinctUntilChanged to the withLatestFrom Observable,
+        val compositeDisposable = CompositeDisposable()
+        val button = PublishSubject.create<Unit>()
+        val editText = PublishSubject.create<String>()
+
+        editText.sample(button).subscribeBy {
+            println(it)
+        }.addTo(compositeDisposable)
+
+        button.onNext(Unit)
+        editText.onNext("a")
+        editText.onNext("ab")
+        button.onNext(Unit)
+        button.onNext(Unit)
+        editText.onNext("abc")
+        button.onNext(Unit)
+    }
+
+    @Test
+    fun ambTest() {
+        //It has a few select practical applications, like connecting to redundant servers and sticking with the one that responds first.
+        val compositeDisposable = CompositeDisposable()
+        val left = PublishSubject.create<String>()
+        val right = PublishSubject.create<String>()
+        left.ambWith(right).subscribeBy {
+            println(it)
+        }.addTo(compositeDisposable)
+
+        right.onNext("right 2")
+        left.onNext("left 1")
+        left.onNext("left 2")
+    }
+
+    @Test
+    fun reduceTest() {
+        val compositeDisposable = CompositeDisposable()
+        val subscription = Observable.just(1, 2, 3, 4, 5)
+        subscription.reduce(0) { a, b ->
+            a + b
+
+        }.subscribeBy {
+            println(it)
+        }.addTo(compositeDisposable)
+
+
+    }
+
+    @Test
+    fun scanTest() {
+        val compositeDisposable = CompositeDisposable()
+        val subscription = Observable.just(1, 2, 3, 4, 5)
+        subscription.scan { a, b ->
+            a + b
+
+        }.subscribeBy {
+            println(it)
+        }.addTo(compositeDisposable)
+    }
+
+    @Test
+    fun zipCaseTest() {
+        //Take the code from the scan example above and improve it so as to display both the current value and the running total at the same time.
+        val compositeDisposable = CompositeDisposable()
+        val source = Observable.just(1, 2, 3, 4, 5)
+
+        val scanObservable = source.scan { a, b ->
+            a + b
+        }
+
+        Observable.zip(source, scanObservable) { value, tatal ->
+            value to tatal
+        }.subscribeBy {
+            println("value:${it.first},total:${it.second}")
+        }.addTo(compositeDisposable)
+
+
+        //second
+        source.scan(0 to 0) { acc, current ->
+            current to acc.second + current
+
+        }.subscribeBy {
+            println("second  value:${it.first},total:${it.second}")
+        }.addTo(compositeDisposable)
+
+    }
+
 }
