fdf8bc489ec2fefeb4cbac324aaeef1dd04ed917
junwang <wang.jun16@163.com>
2024-12-25 06:37:30 +0900

    modify readme.md

--- feature/photo/src/main/kotlin/com/iblogstreet/photo/PhotoMainActivity.kt
+++ feature/photo/src/main/kotlin/com/iblogstreet/photo/PhotoMainActivity.kt
@@ -9,15 +9,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.iblogstreet.designsystem.theme.AppTheme
 import com.iblogstreet.login.expose.LoginExpose
 import dagger.hilt.android.AndroidEntryPoint
@@ -86,10 +94,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun HelloMain() {
+    var name by rememberSaveable { mutableStateOf("") }
+    HelloContent(name, onchange = {
+        name=it
+    })
+}
+
+@Composable
+fun HelloContent(name: String,onchange:(String)->Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        if(name.isNotEmpty()) {
+            Text(
+                text = "Hello! $name",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { onchange(it)},
+            label = { Text("Name") }
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppTheme {
-        Greeting("Android")
+        HelloMain()
     }
 }
--- readme.md
+++ readme.md
@@ -3,13 +3,12 @@
 要求
 
 * フレーム(mvi,mvp,mvvm)のカプセル
-
- *　開発言語　Kotlin
- *　Material3 
- *　 libs.versions.tomlを使う
- *　通信フレーム（Rxjava3,コルーチン)
- *　Android アプリのモジュール化
- *　Hilt を使って、モジュールの間を通信
+* 開発言語　Kotlin
+* Material3 
+* libs.versions.tomlを使う
+* 通信フレーム（Rxjava3,コルーチン)
+* Android アプリのモジュール化
+* Hilt を使って、モジュールの間を通信
 
 
 
